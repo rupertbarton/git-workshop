b7fac127752c9c413d365d60240bb61cd729883d
Rupert Barton <rbarton@scottlogic.com>
2023-01-17 16:09:46 +0000

    ID-40: Changed text to italics

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <i>Edit <code>src/App.js</code> and save to reload.</i>
         </p>
         <a
           className="App-link"
